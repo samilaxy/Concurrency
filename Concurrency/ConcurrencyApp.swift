e9efc0e5ae834aa8ad29beb916fc263137f17bb3
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-04-03 09:01:08 +0000

    Initial Commit

---
+++ Concurrency/ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyApp.swift
+//  Concurrency
+//
+//  Created by Noye Samuel on 03/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
